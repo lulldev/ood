8edc2f59ee4f2e417a7f9f1d2b0b9f4601fd643d
lulldev <lulldev@yandex.ru>
2018-03-02 09:09:21 +0300

    lw3 - добавил все текущие добавки в объект приготовления

--- lw3/Beverages/src/App.ts
+++ lw3/Beverages/src/App.ts
@@ -1,6 +1,6 @@
 import {IBeverage} from "./IBeverage";
 import {Coffee} from "./Beverages";
-import {Lemon} from "./Condiments";
+import {Cinnamon, IceCubeType, Lemon, IceCubes, Syrop, SyrupType, ChocolateCrumbs, CoconutFlakes} from "./Condiments";
 
 const MakeBeverage = {
   beverage: null,
@@ -11,6 +11,31 @@ const MakeBeverage = {
   addLemon: function (quantity: number) {
     const tmpBeverage = this.beverage;
     this.beverage = new Lemon(tmpBeverage, quantity);
+    return this;
+  },
+  addCinnamon: function () {
+    const tmpBeverage = this.beverage;
+    this.beverage = new Cinnamon(tmpBeverage);
+    return this;
+  },
+  addIceCubes: function (quantity: number, iceCubeType: IceCubeType) {
+    const tmpBeverage = this.beverage;
+    this.beverage = new IceCubes(tmpBeverage, quantity, iceCubeType);
+    return this;
+  },
+  addSyrop: function (syropType: SyrupType) {
+    const tmpBeverage = this.beverage;
+    this.beverage = new Syrop(tmpBeverage, syropType);
+    return this;
+  },
+  addChocolateCrumbs: function (mass: number) {
+    const tmpBeverage = this.beverage;
+    this.beverage = new ChocolateCrumbs(tmpBeverage, mass);
+    return this;
+  },
+  addCoconutFlakes: function (mass: number) {
+    const tmpBeverage = this.beverage;
+    this.beverage = new CoconutFlakes(tmpBeverage, mass);
     return this;
   },
   get: function () {
@@ -24,6 +49,7 @@ const beverage = MakeBeverage
   .make(coffee)
   .addLemon(2)
   .addLemon(3)
+  .addCoconutFlakes(10)
   .get();
 
 console.log(beverage.GetDescription());
