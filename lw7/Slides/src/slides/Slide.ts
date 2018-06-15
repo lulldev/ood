da1cab6d08b0bfa3981d909c9e446cd9290bf46a
lulldev <lulldev@yandex.ru>
2018-06-15 16:48:41 +0300

    lw7

--- lw7/Slides/src/slides/Slide.ts
+++ lw7/Slides/src/slides/Slide.ts
@@ -1,13 +1,30 @@
-import { Frame } from './Frame';
+import {ICanvas} from '../canvas/ICanvas';
+import {ISlide} from './ISlide';
+import {IGraphicComponent} from './ISlide';
 
-export class Slide extends Frame {
+export class Slide implements ISlide {
 
   private width: number;
   private height: number;
+  private childrenComponents: IGraphicCopmonent[] = [];
 
   constructor(width: number, height: number) {
-    super();
     this.width = width;
     this.height = height;
+  }
+
+  public AddChildren(children: IGraphicCopmonent): void {
+    // todo: check coord
+    this.childrenComponents.push(children);
+  }
+
+  public RemoveChildren(children: IGraphicCopmonent): void {
+    this.childrenComponents.filter((e: IGraphicCopmonent) => e !== children);
+  }
+
+  public Draw(canvas: ICanvas) {
+    this.childrenComponents.forEach((children: IGraphicCopmonent) => {
+      children.Draw(canvas);
+    });
   }
 }
