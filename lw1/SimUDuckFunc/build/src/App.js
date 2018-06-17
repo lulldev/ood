178bd19579531c45361e157bfb64953680325a10
lulldev <lulldev@yandex.ru>
2018-06-18 02:34:30 +0300

    lw1 - добавил реализацию в стиле ФП

---
+++ lw1/SimUDuckFunc/build/src/App.js
@@ -0,0 +1,96 @@
+"use strict";
+const anything = () => { console.log('Do anything!'); };
+const quack = () => { console.log('Quack Quack!'); };
+const squeek = () => { console.log('Squeek'); };
+const danceWaltz = () => { console.log('I\'m dancing waltz!'); };
+const danceMinuet = () => { console.log('I\'m dancing minuet!'); };
+const notDance = () => { console.log('I\'m not dance!'); };
+const flyWithWingsBehavior = () => {
+    let counter = 0;
+    return () => {
+        counter++;
+        console.log(`I am flying with wings. Flight counter is: ${counter}`);
+    };
+};
+const flyWithRocketEngineBehavior = () => {
+    let counter = 0;
+    return () => {
+        counter++;
+        console.log(`I am flying with rocket engine. Flight counter is: ${counter}`);
+    };
+};
+class Duck {
+    constructor(flyBehavior, quackBehavior, danceBehavior) {
+        this.flyBehavior = flyBehavior;
+        this.quackBehavior = quackBehavior;
+        this.danceBehavior = danceBehavior;
+    }
+    SetFlyBehavior(flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+    Quack() {
+        this.quackBehavior();
+    }
+    Swim() {
+        console.log('I am swiming!');
+    }
+    Fly() {
+        this.flyBehavior();
+    }
+    Dance() {
+        this.danceBehavior();
+    }
+}
+class MallardDuck extends Duck {
+    constructor() {
+        super(flyWithWingsBehavior(), quack, danceWaltz);
+    }
+    Display() {
+        console.log('I\'m mallard duck!');
+    }
+}
+class RedheadDuck extends Duck {
+    constructor() {
+        super(flyWithWingsBehavior(), quack, danceMinuet);
+    }
+    Display() {
+        console.log('I\'m readhead duck!');
+    }
+}
+class DeckoyDuck extends Duck {
+    constructor() {
+        super(anything, anything, notDance);
+    }
+    Display() {
+        console.log('I\'m deckoy duck!');
+    }
+}
+class RubberDuck extends Duck {
+    constructor() {
+        super(anything, squeek, notDance);
+    }
+    Display() {
+        console.log('I\'m rubber duck!');
+    }
+}
+class ModelDuck extends Duck {
+    constructor() {
+        super(anything, quack, notDance);
+    }
+    Display() {
+        console.log('I\'m rubber duck!');
+    }
+}
+const drawDuck = (duck) => {
+    duck.Display();
+};
+const playWithDuck = (duck) => {
+    drawDuck(duck);
+    duck.Quack();
+    duck.Fly();
+    duck.Dance();
+};
+const mallardDuck = new MallardDuck();
+playWithDuck(mallardDuck);
+playWithDuck(mallardDuck);
+//# sourceMappingURL=App.js.map
---
+++ lw1/SimUDuckFunc/src/App.ts
@@ -0,0 +1,155 @@
+type FlyBehavior = () => void;
+type DanceBehavior = () => void;
+type QuackBehavior = () => void;
+
+const anything = () => { console.log('Do anything!'); };
+const quack: QuackBehavior = () => { console.log('Quack Quack!'); };
+const squeek: FlyBehavior = () => { console.log('Squeek'); };
+const danceWaltz: DanceBehavior = () => { console.log('I\'m dancing waltz!'); };
+const danceMinuet: DanceBehavior = () => { console.log('I\'m dancing minuet!'); };
+const notDance: DanceBehavior = () => { console.log('I\'m not dance!'); };
+
+const flyWithWingsBehavior = () => {
+  let counter = 0;
+
+  return () => {
+    counter++;
+    console.log(`I am flying with wings. Flight counter is: ${counter}`);
+  };
+};
+
+const flyWithRocketEngineBehavior = () => {
+  let counter = 0;
+
+  return () => {
+    counter++;
+    console.log(`I am flying with rocket engine. Flight counter is: ${counter}`);
+  };
+};
+
+abstract class Duck {
+
+  private flyBehavior: FlyBehavior;
+  private quackBehavior: QuackBehavior;
+  private danceBehavior: DanceBehavior;
+
+  constructor(flyBehavior: FlyBehavior, quackBehavior: QuackBehavior, danceBehavior: DanceBehavior) {
+    this.flyBehavior = flyBehavior;
+    this.quackBehavior = quackBehavior;
+    this.danceBehavior = danceBehavior;
+  }
+
+  public SetFlyBehavior(flyBehavior: FlyBehavior): void {
+    this.flyBehavior = flyBehavior;
+  }
+
+  public Quack(): void {
+    this.quackBehavior();
+  }
+
+  public Swim(): void {
+    console.log('I am swiming!');
+  }
+
+  public Fly(): void {
+    this.flyBehavior();
+  }
+
+  public Dance(): void {
+    this.danceBehavior();
+  }
+
+  public abstract Display(): void;
+}
+
+class MallardDuck extends Duck {
+
+  constructor() {
+    super(flyWithWingsBehavior(), quack, danceWaltz);
+  }
+
+  public Display(): void {
+    console.log('I\'m mallard duck!');
+  }
+}
+
+class RedheadDuck extends Duck {
+
+  constructor() {
+    super(flyWithWingsBehavior(), quack, danceMinuet);
+  }
+
+  public Display(): void {
+    console.log('I\'m readhead duck!');
+  }
+}
+
+class DeckoyDuck extends Duck {
+
+  constructor() {
+    super(anything, anything, notDance);
+  }
+
+  public Display(): void {
+    console.log('I\'m deckoy duck!');
+  }
+}
+
+class RubberDuck extends Duck {
+
+  constructor() {
+    super(anything, squeek, notDance);
+  }
+
+  public Display(): void {
+    console.log('I\'m rubber duck!');
+  }
+}
+
+class ModelDuck extends Duck {
+
+  constructor() {
+    super(anything, quack, notDance);
+  }
+
+  public Display(): void {
+    console.log('I\'m rubber duck!');
+  }
+}
+
+const drawDuck = (duck: Duck) => {
+  duck.Display();
+};
+
+const playWithDuck = (duck: Duck) => {
+  drawDuck(duck);
+  duck.Quack();
+  duck.Fly();
+  duck.Dance();
+};
+
+const mallardDuck = new MallardDuck();
+
+playWithDuck(mallardDuck);
+playWithDuck(mallardDuck);
+
+// mallardDuck.SetFlyBehavior(MakeFlyWithRocketEngineBehavior());
+// PlayWithDuck(mallardDuck);
+
+// const redheadDuck = new RedheadDuck();
+// playWithDuck(redheadDuck);
+//
+// const rubberDuck = new RubberDuck();
+// playWithDuck(rubberDuck);
+//
+// const deckoyDuck = new DeckoyDuck();
+// playWithDuck(deckoyDuck);
+//
+// const modelDuck = new ModelDuck();
+// playWithDuck(modelDuck);
+
+// Однако можно нацепить на неё крылья и она полетит, подсчитывая количество полётов
+// modelDuck.SetFlyBehavior(MakeFlyWithWingsBehavior());
+// PlayWithDuck(modelDuck);
+// modelDuck.SetFlyBehavior(MakeFlyWithRocketEngineBehavior());
+// PlayWithDuck(modelDuck);
